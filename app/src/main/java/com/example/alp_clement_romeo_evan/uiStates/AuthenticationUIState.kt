66ae8d83a1c416a769bff2b8a7591f5b02de912e
Clem-prog <clementisjs747@gmail.com>
2025-01-03 21:55:17 +0700

    user things added LOLOL

--- app/src/main/java/com/example/alp_clement_romeo_evan/uiStates/AuthenticationUIState.kt
+++ app/src/main/java/com/example/alp_clement_romeo_evan/uiStates/AuthenticationUIState.kt
@@ -1,4 +1,15 @@
 package com.example.alp_clement_romeo_evan.uiStates
 
-interface AuthenticationUIState {
-}
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.example.alp_clement_romeo_evan.R
+
+data class AuthenticationUIState(
+    val showPassword: Boolean = false,
+    val showConfirmPassword: Boolean = false,
+    val passwordVisibility: VisualTransformation = PasswordVisualTransformation(),
+    val confirmPasswordVisibility: VisualTransformation = PasswordVisualTransformation(),
+    //val passwordVisibilityIcon: Int = R.drawable.ic_password_visible,
+    //val confirmPasswordVisibilityIcon: Int = R.drawable.ic_password_visible,
+    val buttonEnabled: Boolean = false
+)
